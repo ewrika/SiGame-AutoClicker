404cd043a4e363eab43f4597d484750adecf8797
ewrika <61907186+ewrika@users.noreply.github.com>
2023-02-05 19:59:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,51 @@ import numpy as np
 from mss import mss
 import pyautogui as pg
 import keyboard
+from win32api import GetSystemMetrics
+global monitor
+print('Working')
+monitor={}
 
-# Set up the monitor region to search
-monitor = {
-    "left": 361,
-    "top": 585,
-    "width": 9,
-    "height": 59,
-}
+def Mons():
+    Mon = str(GetSystemMetrics(0)) + 'x' + str(GetSystemMetrics(1))
+    if Mon == "1920x1080":
+        monitors = {
+            "left": 361,
+            "top": 585,
+            "width": 9,
+            "height": 59,
+        }
+        monitor.update(monitors)
 
+    elif Mon == "1280x720":
+        monitors = {
+            "left": 247,
+            "top": 425,
+            "width": 9,
+            "height": 59,
+        }
+        monitor.update(monitors)
+    elif Mon == "1366x768":
+        monitors = {
+            "left": 262,
+            "top": 462,
+            "width": 9,
+            "height": 59,
+        }
+        monitor.update(monitors)
+    elif Mon == "1440x900":
+        monitors = {
+            "left": 274,
+            "top": 507,
+            "width": 9,
+            "height": 59,
+        }
+        monitor.update(monitors)
+    else:
+        print('Ваше разрешение экрана не подходит')
 # Set the color to search for
 our_color = [255, 255, 255]
-
+Mons()
 # Function to find the color on the screen
 def find_color(our_color, monitor={}):
     #Поиск цвета на экране
@@ -49,5 +82,4 @@ while True:
     if result.__len__():
         x = result[0][1] + monitor.get("left")
         y = result[0][0] + monitor.get("top")
-        pg.moveTo(x, y)  # Move to the found color
         keyboard.send("ctrl")
